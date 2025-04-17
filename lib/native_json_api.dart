4627a1f77823223a7e707417d2c2878bf47552fb
JavaCommons Technologies <javacommons@gmail.com>
2025-04-18 00:22:45 +0900

    .

--- lib/native_json_api.dart
+++ lib/native_json_api.dart
@@ -1,8 +1,53 @@
-// *ignore_for_file*: camel_case_types
-// *ignore_for_file*: type=lint
+import 'dart:convert' as convert;
+import 'dart:ffi' as ffi;
+import 'package:ffi/ffi.dart' as ffi;
+import 'package:dynamic_function/dynamic_function.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class NativeJsonApi {
+  final String $$dllName;
+  late final ffi.DynamicLibrary $$lib;
+  late final ffi.Pointer<ffi.Utf8> Function(
+    ffi.Pointer<ffi.Utf8>,
+    ffi.Pointer<ffi.Utf8>,
+  )
+  $$callFunc;
+
+  NativeJsonApi(this.$$dllName) {
+    $$lib = ffi.DynamicLibrary.open($$dllName);
+    $$callFunc =
+        $$lib
+            .lookup<
+              ffi.NativeFunction<
+                ffi.Pointer<ffi.Utf8> Function(
+                  ffi.Pointer<ffi.Utf8>,
+                  ffi.Pointer<ffi.Utf8>,
+                )
+              >
+            >('Call')
+            .asFunction();
+  }
+
+  dynamic call(String $name, dynamic $args) {
+    String $input = convert.jsonEncode($args);
+    final $namePtr = $name.toNativeUtf8();
+    final $inputPtr = $input.toNativeUtf8();
+    dynamic $result = convert.jsonDecode(
+      $$callFunc($namePtr, $inputPtr).toDartString(),
+    );
+    ffi.calloc.free($namePtr);
+    ffi.calloc.free($inputPtr);
+    if ($result is String) {
+      throw $result;
+    }
+    return $result[0];
+  }
+
+  late final dynamic call$ = DynamicFunction((
+    List<dynamic> $positional,
+    Map<Symbol, dynamic> $named,
+  ) {
+    String $name = $positional[0];
+    List $rest = $positional.sublist(1);
+    return call($name, $rest);
+  });
 }
--- test/run01.dart
+++ test/run01.dart
@@ -2,7 +2,14 @@ import 'package:output/output.dart';
 import 'package:native_json_api/native_json_api.dart';
 
 void main() {
-  var calc = new Calculator();
-  var result = calc.addOne(123);
-  dump(result, 'result');
+  final $api = NativeJsonApi('PROGRAM.dll');
+  try{
+    $api.call('system', ['ping', '-n', '2', 'www.youtube.com']);
+    dump($api.call('add2', [11, 22]));
+    dump($api.call$('add2', 111, 222));
+    dump($api.call('add2', [11, 22, 33]));
+  } catch (e, stacktrace) {
+    dump('Exception: $e');
+    dump('Stacktrace: $stacktrace');
+  }
 }
